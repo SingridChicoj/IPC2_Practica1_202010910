159d4ccf03c7889b63a75a06348ecb27ad498428
Singrid Martinez <3403252160101@ingenieria.usac.edu.gt>
2023-09-10 23:33:59 -0600

    Commit

--- app.py
+++ app.py
@@ -2,27 +2,34 @@ from lista_piezas import lista_piezas
 
 
 mi_tablero = lista_piezas()
-
+jump = "---------------Coloréalo---------------"
 
 def configuracion_tablero():
-    print("")
-    print("")
-    print("=============================CONFIGURACIÓN TABLERO=================================")
-    filas = input("Ingrese el número de filas: ")
-    columnas = input("Ingrese el número de columnas: ")
+    filas = input("Ingrese el ancho del tablero: ")
+    columnas = input("Ingrese el alto del tablero: ")
     # Creo todas las piezas de mi tablero
     mi_tablero.inicializar_tablero(int(filas), int(columnas))
     mi_tablero.filas = int(filas)
     mi_tablero.columnas = int(columnas)
     # Verificamos que mi tablero se haya creado correctamente
-    mi_tablero.recorrer_imprimir()
+    #mi_tablero.imprimir_tablero_consola()
     # Sentinela de agregar nueva pieza
-    print("=============================CONFIGURACIÓN PIEZAS=================================")
+    print(jump)
     nueva_pieza = True
     while nueva_pieza:
-        fila = input("Ingrese la fila de la pieza: ")
-        columna = input("Ingrese la columna de la pieza: ")
-        color = input("Ingrese el color de la pieza: ")
+        print("Por favor elija su color", "\n",
+              "* BLUE", "\n",
+              "* RED", "\n",
+              "* GREEN", "\n",
+              "* VIOLET", "\n",
+              "* ORANGE", "\n",)
+        print()
+        print("Por favor ingrese la fila en que desea colocar la pieza")
+        fila = input("Fila: ")
+        print("Por favor ingrese la columna en que desea colocar la pieza")
+        columna = input("Columna: ")
+        color = input("Color: ")
+        print(color, ":")
         mi_tablero.actualizar_pieza(int(fila), int(columna), color)
         print("")
         print("")
@@ -33,32 +40,51 @@ def configuracion_tablero():
         print("")
         if respuesta == "N" or respuesta == "n":
             nueva_pieza = False
-    print("=============================FIN CONFIGURACIÓN PIEZAS=================================")
+    print(jump)
     mi_tablero.imprimir_tablero_consola()
-    print("=============================FIN CONFIGURACIÓN TABLERO=================================")
+    print(jump)
     print("")
     print("")
     # Deberiamos graficar
     mi_tablero.graficar()
 
 
-def mostrar_menu():
+def menu():
     print("")
     print("")
-    print("-----------------------Menú -----------------------")
-    print("1. Configurar tablero")
-    print("2. Salir")
-    opcion = input("Ingrese una opción válida del menú: ")
-    while True:
-        if opcion == "1":
-            print("")
-            configuracion_tablero()
-            break
-        elif opcion == "2":
-            print("Hasta la próxima")
-            break
-        else:
-            print("Indique una opción válida")
+    print(jump)
+    print("1. Crear tablero")
+    print("2. Mostrar datos del estudiante")
+    print("3. Salir")
+    entrada = input("Ingrese una opción válida del menú: ")
+    if entrada == "3":
+        print("Adios, regresa pronto")
+        quit()
+    else:
+        while entrada != "3":
+            if entrada == "1":
+                print("")
+                print(jump)
+                entrada = ""
+                configuracion_tablero()
+                menu()
+            elif entrada == "2":
+                print("")
+                print(jump)
+                print("Datos del estudiante: ")
+                print(" * Singrid Cristabel Chicoj Martinez", "\n",
+                      "* 202010910",  "\n",
+                      "* Introduccion a la Programacion y", "\n", "  Computacion 2", "\n",
+                      "* Seccion D", "\n",
+                      "* 4to Semestre y Parte del 5to Semeste :c")
+                entrada = ""
+                print("")
+                menu()
+            else:
+                print("")
+                print("Seleccione una opcion correcta")
+                entrada = ""
+                print("")
+                menu()
 
-
-mostrar_menu()
+menu()
--- lista_piezas.py
+++ lista_piezas.py
@@ -37,7 +37,7 @@ class lista_piezas:
     def inicializar_tablero(self, filas, columnas):
         for i in range(1, filas + 1):
             for j in range(1, columnas + 1):
-                self.insertar_dato(pieza(i, j, "White"))
+                self.insertar_dato(pieza(i, j, " "))
 
     def actualizar_pieza(self, fila, columna, color):
         actual = self.primero
@@ -55,26 +55,55 @@ class lista_piezas:
             if actual.pieza.fila == fila and actual.pieza.columna == columna:
                 return actual.pieza.color
             actual = actual.siguiente
+    
+    '''def devolver_color_de_pieza(self, fila, columna):
+        actual = self.primero
+        while actual != None:
+            if actual.pieza.fila == fila and actual.pieza.columna == columna:
+                if actual.pieza.color == "BLUE" or actual.pieza.color == "blue":
+                    actual.pieza.color = 'B'
+                    return actual.pieza.color
+                elif actual.pieza.color == "RED" or actual.pieza.color == "red":
+                    actual.pieza.color = 'R'
+                    return actual.pieza.color
+                elif actual.pieza.color == "GREEN" or actual.pieza.color == "GREEN":
+                    actual.pieza.color = 'G'
+                    return actual.pieza.color
+                elif actual.pieza.color == "VIOLET" or actual.pieza.color == "violet":
+                    actual.pieza.color = 'V'
+                    return actual.pieza.color
+                elif actual.pieza.color == "ORANGE" or actual.pieza.color == "orange":
+                    actual.pieza.color = 'O'
+                    return actual.pieza.color
+                else:
+                    actual.pieza.color = ' '
+                    return actual.pieza.color
+            actual = actual.siguiente'''
 
     def imprimir_tablero_consola(self):
+        recolector_texto = ""
         for i in range(1, self.filas + 1):
             for j in range(1, self.columnas + 1):
-                print(self.devolver_color_de_pieza(i, j), end="\t")
+                recolector_texto += "| " + str(self.devolver_color_de_pieza(i, j)) + " "
+            recolector_texto += "|"
+            print(recolector_texto)
+            recolector_texto = ""
             print("")
         print("")
 
     def graficar(self):
         f = open('IPC2_Practica1_202010910/aa.dot', 'w')
-        texto = "digraph G {\n node [shape=plaintext];\nlabel=\"Tablero facilito\";\nsome_node [\nlabel=<\n<table border=\"0\" cellborder=\"0\" cellspacing=\"0\" width=\"100%\" height=\"100%\">\n"
+        texto = "digraph G {\n node [shape=rectangle];\nlabel=\"Tablero\";\nsome_node[\nlabel=<\n<table border=\"0\" cellspacing=\"10\" cellpadding=\"10\"  width=\"100%\" height=\"100%\">\n"
+        #texto = "digraph G {\n node [shape=plaintext];\nlabel=\"Tablero facilito\";\nsome_node [\nlabel=<\n<table border=\"0\" cellborder=\"0\" cellspacing=\"0\" width=\"100%\" height=\"100%\">\n"
         for i in range(1, self.filas + 1):
             texto += "<tr>\n"
             for j in range(1, self.columnas + 1):
                 texto += "<td bgcolor=\""+self.devolver_color_de_pieza(
-                    i, j)+"\" width=\"1\" height=\"1\">"+str(i)+","+str(j)+"</td>\n"
+                    i, j)+"\" width=\"1\" height=\"1\">"+str(self.devolver_color_de_pieza(i, j))+"</td>\n"
             texto += "</tr>\n"
         texto += "</table>>\n];\n}"
         f.write(texto)
         f.close()
         os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
         os.system('dot -Tpng IPC2_Practica1_202010910/aa.dot -o IPC2_Practica1_202010910/Tablero.png')
-        print("Tablero facilito")
+        print("Tablero Creado")
